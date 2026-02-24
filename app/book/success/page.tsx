a48119c5792132ece1cbdba0ff204d38ed40007d
misstechxter <misstechxter@gmail.com>
2026-02-24 20:49:48 +0000

    fix: add default exports to all page files

---
+++ app/book/success/page.tsx
@@ -0,0 +1,3 @@
+export default function BookSuccessPage() {
+  return <div>Booking Confirmed!</div>;
+}
---
+++ app/prep-reminders/page.tsx
@@ -0,0 +1,3 @@
+export default function PrepRemindersPage() {
+  return <div>Prep & Reminders</div>;
+}
